7d5798f4690555398a1fc8c19a2dd13edba54926
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-11-21 12:21:09 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,21 @@
+enum RadioMessage {
+    message1 = 49434,
+    Sike = 10816
+}
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(2)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(3)
+})
+radio.setGroup(1)
+basic.showLeds(`
+    . . # . .
+    # # # # #
+    # . # . #
+    . # # . .
+    # # . # #
+    `)
